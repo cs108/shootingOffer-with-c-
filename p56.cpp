3ed725dd58681b3614a5a7a8864e50d904dba1cc
cs108 <41218033+cs108@users.noreply.github.com>
2019-03-23 11:09:03 +0800

    Update p56.cpp

--- p56.cpp
+++ p56.cpp
@@ -7,31 +7,6 @@ struct ListNode
 	ListNode *next;
 };
 
-
-/*  given by the book
-void AddToTail(ListNode **head, int value)
-{
-	ListNode *newNode = new ListNode();
-	newNode->next = nullptr;
-	newNode->value = value;
-
-	if (*head == nullptr)
-	{
-		*head = newNode;
-
-	}
-	else
-	{
-		ListNode *pNode = *head;
-		while (pNode->next != nullptr)
-		{
-			pNode = pNode->next;
-		}
-		pNode->next = newNode;
-	}
-}
-*/
-
 //another way written by me
 void AddToTail(ListNode *&head, int value)
 {
@@ -69,3 +44,54 @@ int main()
 		(p->next == nullptr) ? cout << '.' << endl : cout << ',';//this is the instruction
 	}
 }
+
+
+
+//following is given by the book except main()
+/*
+#include<iostream>
+using namespace std;
+
+struct ListNode
+{
+	int value;
+	ListNode *next;
+};
+
+// given by the book
+void AddToTail(ListNode **head, int value)
+{
+	ListNode *newNode = new ListNode();
+	newNode->next = nullptr;
+	newNode->value = value;
+	if (*head == nullptr)
+	{
+		*head = newNode;
+	}
+	else
+	{
+		ListNode *pNode = *head;
+		while (pNode->next != nullptr)
+		{
+			pNode = pNode->next;
+		}
+		pNode->next = newNode;
+	}
+}
+
+int main()
+{
+	ListNode **L;
+	ListNode *l = nullptr;  //something like l must be declare for initializing L
+	L = &l;                 //there would be problem if you use L=nullptr to initialize L 
+	AddToTail(L, 0);
+	AddToTail(L, 1);
+	cout << "values:";
+	for (ListNode *p = *L; p != nullptr; p = p->next)
+	{
+		cout << p->value;
+		//cout <<( p->next == nullptr )? '.' : ','; //this would cause something wrong logically
+		(p->next == nullptr) ? cout << '.' << endl : cout << ',';//this is the instruction
+	}
+}
+*/
